9c5bf115579d625afb6e337ffeb77c2a1822a482
Saul Enrique Segovia Hernandez <saul.segovia@softtek.com>
2020-11-03 15:22:15 -0600

    -Adding EntityBase, Role and User Models -Adding and setup EF Core -Downgrade to Net Core 2.1 -Changes to API Project to support Net Core 2.1

---
+++ AutomotiveCenter.Application/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using AutomotiveCenter.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AutomotiveCenter.Application.Models
+{
+    public class User: EntityBase
+    {
+        [Key]
+        public int userId { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+
+        [ForeignKey("role")]
+        public int roleId { get; set; }
+
+        public Role role { get; set; }
+    }
+}
